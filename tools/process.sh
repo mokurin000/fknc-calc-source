7e040294f03cd4df4392b12dcedbadaff65a4074
mokurin000 <mokurin000@gmail.com>
2026-02-16 04:50:59 +0800

    enhance: remove useless files

--- tools/process.sh
+++ tools/process.sh
@@ -2,8 +2,11 @@
 
 SCRIPT_ROOT=$(dirname "$0")
 FILENAME=index.js
+SCRIPT=export.js
 
 curl -sSL https://www.fknc.top/assets/index-CIyMZ-Re.js -o "${FILENAME}"
 
 PYTHONUTF8=1 python "${SCRIPT_ROOT}/extract.py"
-node export.js
+node "${SCRIPT}"
+
+rm -rf "${FILENAME}" "${SCRIPT}"
