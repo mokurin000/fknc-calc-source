6704444ecef0f57f2ce0bb40815e0e7c3a0a4ed8
莯凛 <mokurin000@gmail.com>
2026-02-21 17:16:49 +0800

    chore: update to v7.3.2 data

--- tools/process.sh
+++ tools/process.sh
@@ -4,7 +4,7 @@ SCRIPT_ROOT=$(dirname "$0")
 FILENAME=index.js
 SCRIPT=export.js
 
-curl -sSL https://www.fknc.top/assets/index-CIyMZ-Re.js -o "${FILENAME}"
+curl -sSL https://www.fknc.top/assets/index-oOwWjGI1.js -o "${FILENAME}"
 
 PYTHONUTF8=1 python "${SCRIPT_ROOT}/extract.py"
 node "${SCRIPT}"
