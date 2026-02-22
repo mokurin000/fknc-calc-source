1cb20099719f0cdc7979fc392e3d3baece4bb52d
莯凛 <mokurin000@gmail.com>
2026-02-22 13:28:01 +0800

    chore: sync co-efficient of 红包果

--- tools/process.sh
+++ tools/process.sh
@@ -4,7 +4,7 @@ SCRIPT_ROOT=$(dirname "$0")
 FILENAME=index.js
 SCRIPT=export.js
 
-curl -sSL https://www.fknc.top/assets/index-oOwWjGI1.js -o "${FILENAME}"
+curl -sSL https://www.fknc.top/assets/index-BKh7obpC.js -o "${FILENAME}"
 
 PYTHONUTF8=1 python "${SCRIPT_ROOT}/extract.py"
 node "${SCRIPT}"
