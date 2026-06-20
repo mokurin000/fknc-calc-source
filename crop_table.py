d55eeb59124d616580b1379243ab6f4f74db4f49
莯凛 <mokurin000@gmail.com>
2026-06-20 15:48:05 +0800

    chore: update crop data

--- crop_table.py
+++ crop_table.py
@@ -1,7 +1,9 @@
 from fknc_calc import load_data
 
 crops, _ = load_data()
-print("作物 | 价格系数 | 最大重量")
-print("---: | :---: | :---")
+print("作物 | 价格系数 | 最大重量 | 种子价值")
+print("---: | :---: | :--- | :--:")
 for crop in crops:
-    print(f"{crop.name} | {crop.price_coefficient:.4f} | {crop.max_weight}")
+    print(
+        f"{crop.name} | {crop.price_coefficient:.4f} | {crop.max_weight} | {crop.seed_price}"
+    )
